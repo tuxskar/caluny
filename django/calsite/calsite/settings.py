0ee44e761fcaa7ba1a27eceb576b9777e3e084db
Oscar Ramirez <tuxskar@gmail.com>
2014-09-04 23:34:30 +0200

    Added djangorestframework

--- django/calsite/calsite/settings.py
+++ django/calsite/calsite/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = (
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'caluny',
+    'rest_framework',
 )
 
 MIDDLEWARE_CLASSES = (
--- django/calsite/calsite/urls.py
+++ django/calsite/calsite/urls.py
@@ -6,5 +6,6 @@ urlpatterns = patterns('',
     # url(r'^$', 'calsite.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
 
+    url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^', include(admin.site.urls)),
 )
