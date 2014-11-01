9ea1d72315db3e3372146fc931c9da989fd49918
Oscar Ramirez <tuxskar@gmail.com>
2014-11-01 14:14:54 +0100

    Added dummydb in fixtures and api working by token

--- django/calsite/calsite/settings.py
+++ django/calsite/calsite/settings.py
@@ -52,10 +52,10 @@ INSTALLED_APPS = (
 REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         #'rest_framework.authentication.BasicAuthentication',
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        'rest_framework.authentication.SessionAuthentication',
     ),
-    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAdminUser',),
+    'DEFAULT_PERMISSION_CLASSES': ('rest_framework.permissions.IsAuthenticated',),
     'PAGINATE_BY': 10
 }
 
