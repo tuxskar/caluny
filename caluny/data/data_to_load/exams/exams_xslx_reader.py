95ead3ed2563c83cfe7551bbb520f931e0b7a2e3
Oscar Ramirez <tuxskar@gmail.com>
2015-09-29 22:57:22 +0200

    Added exam automatically using xlsx files

--- caluny/data/data_to_load/exams/exams_xslx_reader.py
+++ caluny/data/data_to_load/exams/exams_xslx_reader.py
@@ -1,62 +1,63 @@
 # coding=utf-8
-import datetime
+
 import os
 
 os.environ['DJANGO_SETTINGS_MODULE'] = 'caluny.settings.common'
-
 from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
 
+import datetime
 from os.path import join, dirname, abspath
 import xlrd
 
-from core.models import SemesterDate
+from core.models import School, University, Subject, Exam, TeachingSubject, Degree
 
+import locale
+
+locale.setlocale(locale.LC_ALL, 'es_ES')
 degree_names = [
     u'Grado en Ingeniería Informática',
     u'Grado en Ingeniería de Computadores',
     u'Grado en Ingeniería del Software',
     u'Grado en Ingeniería de la Salud'
 ]
-
-first_semester_start_date = '2015-09-29'
-first_semester_end_date = '2016-02-16'
-second_semester_start_date = '2016-02-22'
-second_semester_end_date = '2016-07-01'
-
-first_semester_start = SemesterDate(date=datetime.datetime.strptime(first_semester_start_date, '%Y-%m-%d').date())
-first_semester_end = SemesterDate(date=datetime.datetime.strptime(first_semester_end_date, '%Y-%m-%d').date())
-second_semester_start = SemesterDate(date=datetime.datetime.strptime(second_semester_start_date, '%Y-%m-%d').date())
-second_semester_end = SemesterDate(date=datetime.datetime.strptime(second_semester_end_date, '%Y-%m-%d').date())
-print first_semester_start
-print first_semester_end
+university, _ = University.objects.get_or_create(name=u'Universidad de Málaga',
+                                                 address=u'Avda. Cervantes, 2, 29071',
+                                                 city=u'Málaga')
+school, _ = School.objects.get_or_create(name=u"Escuela Técnica Superior de Ingeniería Informática",
+                                         university=university)
 
 for degree_name in degree_names:
-    fname = join(dirname(abspath(__file__)), degree_name + '.xlsx')
+    print "Analysing " + degree_name
+    file_name = join(dirname(abspath(__file__)), u'Examenes ' + degree_name + u' 2015-2016.xlsx')
     # Open the workbook
-    xl_workbook = xlrd.open_workbook(fname)
+    degree, _ = Degree.objects.get_or_create(title=degree_name, school=school)
+    # Open the workbook
+    xl_workbook = xlrd.open_workbook(file_name)
     # List sheet names, and pull a sheet by name
-    courses_names = xl_workbook.sheet_names()
-    print('Courses for this Degree', courses_names)
-
-    for course_name in courses_names:
-        print u"Processing the course {}".format(course_name)
-        xl_sheet = xl_workbook.sheet_by_name(course_name)
-        from xlrd.sheet import ctype_text
-
-        print('(Column #) type:value')
-        row = xl_sheet.row(0)  # 1st row
-        for idx, cell_obj in enumerate(row):
-            cell_type_str = ctype_text.get(cell_obj.ctype, 'unknown type')
-            print('(%s) %s %s' % (idx, cell_type_str, cell_obj.value))
-
-        # Print all values, iterating through rows and columns
-        #
-        num_cols = xl_sheet.ncols  # Number of columns
-        for row_idx in range(0, xl_sheet.nrows):  # Iterate through rows
-            print ('-' * 40)
-            print ('Row: %s' % row_idx)  # Print row number
-            for col_idx in range(0, num_cols):  # Iterate through columns
-                cell_obj = xl_sheet.cell(row_idx, col_idx)  # Get cell object by row, col
-                print ('Column: [%s] cell_obj: [%s]' % (col_idx, cell_obj))
+    dates_names = xl_workbook.sheet_names()
+    for date_name in dates_names:
+        xl_sheet = xl_workbook.sheet_by_name(date_name)
+        base_date = datetime.datetime.strptime(date_name, '%B %Y')
+        for row_idx in range(1, xl_sheet.nrows):
+            subject_name, date, starting_hour, exam_address = xl_sheet.row(row_idx)
+            try:
+                subject = Subject.objects.get(title=subject_name.value, degree=degree)
+            except Subject.DoesNotExist:
+                continue
+            days = int(date.value.split('-')[0])
+            hours, minutes = map(int, starting_hour.value.split('.'))
+            starting_date = base_date + datetime.timedelta(days=days, hours=hours, minutes=minutes)
+            end_date = starting_date + datetime.timedelta(hours=3)
+            title = u'Examen de ' + subject.title
+            description = title + u' en ' + unicode(exam_address.value)
+            for t_subject in TeachingSubject.objects.filter(subject=subject):
+                exam, _ = Exam.objects.get_or_create(title=title,
+                                                     description=description,
+                                                     address=exam_address.value,
+                                                     date=starting_date.date(),
+                                                     start_time=starting_date.time(),
+                                                     end_time=end_date.time(),
+                                                     t_subject=t_subject)
+                print '    {exam}'.format(exam=exam)
